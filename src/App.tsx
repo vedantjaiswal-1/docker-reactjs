a2db4368c76bf2cd87f07c557e64580e62e043a5
vedant <vedantjaiswal1@gmail.com>
2022-05-26 13:03:29 +0530

    change heading in app

--- src/App.tsx
+++ src/App.tsx
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Learn how to dockerize the react app
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Docker-react
         </a>
       </header>
     </div>
